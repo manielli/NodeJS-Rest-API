46ef827a85a5a8a2242ccf525406b779682fc7a4
Mani Jafari <manielli@gmail.com>
2019-04-25 10:12:58 -0700

    Renamed client.js to db.js in db folder, added methods to create and list all items in database

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-
+const methodOverride = require('method-override');
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(methodOverride((req, res) => {
+    if (req.body && req.body._method) {
+        const method = req.body._method;
+        return method;
+    }
+}));
+
+const PORT = 3000;
+const HOST = 'localhost';
+app.listen(PORT, HOST, () => {
+    console.log(`Server is listening on http://${HOST}:${PORT}`);
+});
--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const knexfile = require('../knexfile');
 const knexConnector = require('knex');
 const knex = knexConnector(knexfile.development);
 
-module.exports = knex;
+// module.exports = knex;
 
 // Or...
 // const knex = require('knex')({
@@ -18,3 +18,14 @@ module.exports = knex;
 //     }
 // })
 // module.exports = knex;
+
+const create = (movie, released) => 
+    knex('movies').insert({title, released});
+
+const list = () => knex('movies').select('*');
+
+module.exports = {
+    knex, 
+    create,
+    list    
+};
